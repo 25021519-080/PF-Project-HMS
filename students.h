231595c7a983c0c8d2f8ef366d23c0d9b5c6dbd5
25021519-080 <shahzaibali4323@gmail.com>
2025-12-11 11:00:00 +0500

    added view and remove student functionality

--- students.h
+++ students.h
@@ -8,6 +8,7 @@
 using namespace std;
 
 void saveUsers();
+void saveRooms();
 bool isUsernameUnique(string username);
 
 int findStudentByRollNo(string rollNo) {
@@ -66,38 +67,46 @@ void admitStudent() {
         return;
     }
     
+    // ask for full name 
     cout << "Full Name: ";
     string fullName;
     getline(cin, fullName);
     
 
+    // ask for username
     cout << "Username (for login): ";
     string username;
     getline(cin, username);
     
+    // Check if username is unique 
     if (!isUsernameUnique(username)) {
         cout << "Username already exists! Please create a unique one." << endl;
         return;
     }
     
+    // ask degree
     cout << "Degree: ";
     string degree;
     getline(cin, degree);
     
+    // ask semester
     cout << "Semester (1-8): ";
     int semester;
     cin >> semester;
     cin.ignore(1000, '\n');
     
+    // check if sem is between 1-8
     if (semester < 1 || semester > 8) {
         cout << "Invalid semester! Must be between 1-8." << endl;
         return;
     }
     
+    // ask for roll no
     cout << "Roll Number: ";
     string rollNo;
     getline(cin, rollNo);
     
+    // see if this roll no exists or no
     for (int i = 0; i < studentCount; i++) {
         if (students[i].rollNo == rollNo) {
             cout << "Student with this roll number already exists!" << endl;
@@ -105,6 +114,9 @@ void admitStudent() {
         }
     }
     
+    // if all the above is done, add the student with 
+    // studentCount as the index, kyunke its always one above the 
+    // stundets array index
     students[studentCount].username = username;
     students[studentCount].fullName = fullName;
     students[studentCount].rollNo = rollNo;
@@ -112,21 +124,129 @@ void admitStudent() {
     students[studentCount].semester = semester;
     students[studentCount].room = 0;
     
+    // increase the student count by one
     studentCount++;
+    // save the students
     saveStudents();
     
+    // new students ke liye automatically aik  user create karlo
     if (userCount < MAX_USERS) {
         users[userCount].username = username;
         // password will be set on the login
         users[userCount].password = "";
         users[userCount].role = STUDENT;
+        // increase it by one
         userCount++;
+        // save the new user data
         saveUsers();
         cout << "Student admitted! Username: " << username << " | Full Name: " << fullName << " | Role: Student" << endl;
         cout << "Student will set password on first login." << endl;
     } else {
         cout << "Student admitted! But user account could not be created (user limit reached)." << endl;
     }
+}
+
+void viewStudents() {
+    if (studentCount == 0) {
+        cout << "No students!" << endl;
+        return;
+    }
+
+// iterate through the studentcount, and show data    
+    cout << "\n=== Students ===" << endl;
+    for (int i = 0; i < studentCount; i++) {
+        cout << "Username: " << students[i].username << " | Name: " << students[i].fullName 
+             << " | Roll No: " << students[i].rollNo << " | " << students[i].degree << " | Sem: " << students[i].semester
+             << " | Room: " << students[i].room << endl;
+    }
+}
+
+void removeStudent() {
+    cout << "Roll Number to remove: ";
+    string rollNo;
+    getline(cin, rollNo);
+    
+
+    // get student index from the function 
+    int studentIndex = findStudentByRollNo(rollNo);
+    if (studentIndex == -1) {
+        cout << "Student not found!" << endl;
+        return;
+    }
+    
+    cout << "Are you sure you want to remove " << students[studentIndex].fullName << "? (yes/no): ";
+    string confirm;
+    getline(cin, confirm);
+    
+    if (confirm != "yes") {
+        cout << "Cancelled!" << endl;
+        return;
+    }
+    
+    string usernameToRemove = students[studentIndex].username;
+    
+    // Remove user account 
+    int userIdx = -1;
+    for (int i = 0; i < userCount; i++) {
+        // first get user idx
+        if (users[i].username == usernameToRemove) {
+            userIdx = i;
+            break;
+        }
+    }
+    // if user idx not -1, shift the array left on one side
+    // kyunke ab jo empty array hogya baqi students wahan par
+    // jagah le lain gay
+    /* if we have:
+    user[0] = aman
+    user[1] = sania
+    user[2] = afaan,
+    if we remove sania, then afaan should have index
+    [1] instead of [2]
+    */
+    if (userIdx != -1) {
+        for (int j = userIdx; j < userCount - 1; j++) {
+            users[j] = users[j + 1];
+        }
+        // decrement of 1
+        userCount--;
+        // save new user data
+        saveUsers();
+    }
+    
+    // Remove room assignment 
+    for (int i = 0; i < roomCount; i++) {
+        // get data of al students
+        string studentSlots[4] = {
+            rooms[i].student1,
+            rooms[i].student2,
+            rooms[i].student3,
+            rooms[i].student4
+        };
+        
+        for (int j = 0; j < 4; j++) {
+            // remove from that specific slot
+            if (studentSlots[j] == rollNo) {
+                if (j == 0) rooms[i].student1 = "";
+                else if (j == 1) rooms[i].student2 = "";
+                else if (j == 2) rooms[i].student3 = "";
+                else if (j == 3) rooms[i].student4 = "";
+                rooms[i].occupancy--;
+                break;
+            }
+        }
+    }
+    // now save the rooms
+    if (roomCount > 0) saveRooms();
+    
+    // jis trah users ko shift kiya usi trah students ko bhi
+    for (int j = studentIndex; j < studentCount - 1; j++) {
+        students[j] = students[j + 1];
+    }
+    // decrement of one
+    studentCount--;
+    saveStudents();
+    cout << "Student removed! (User account and room assignment cleaned up)" << endl;
 }
 
 #endif
