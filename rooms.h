540b2ffde4f846d39645e9d71937c65b8488ca57
IamAman-dev <amantrader480@gmail.com>
2025-12-11 14:00:00 +0500

    load aur save functions add kye isRoomFull logic improve kardi

--- rooms.h
+++ rooms.h
@@ -1,6 +1,7 @@
 #ifndef ROOMS_H
 #define ROOMS_H
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -24,6 +25,33 @@ bool isRoomFull(int roomNo) {
     int idx = findRoomByNumber(roomNo);
     if (idx == -1) return true;
     return rooms[idx].occupancy >= 4;
+}
+
+void loadRooms() {
+    ifstream file("rooms.txt");
+    if (!file.is_open()) return;
+    
+    file >> roomCount;
+    for (int i = 0; i < roomCount; i++) {
+        file >> rooms[i].roomNo >> rooms[i].occupancy;
+        file.ignore();
+        for (int j = 0; j < 4; j++) {
+            getline(file, rooms[i].students[j]);
+        }
+    }
+    file.close();
+}
+
+void saveRooms() {
+    ofstream file("rooms.txt");
+    file << roomCount << endl;
+    for (int i = 0; i < roomCount; i++) {
+        file << rooms[i].roomNo << " " << rooms[i].occupancy << endl;
+        for (int j = 0; j < 4; j++) {
+            file << rooms[i].students[j] << endl;
+        }
+    }
+    file.close();
 }
 
 #endif
